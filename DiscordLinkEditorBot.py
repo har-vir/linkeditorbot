ccc8f17c78ce2d1b3a79cb73bd7493fcaadc6101
har_vir <nijjarh@oregonstate.edu>
2024-02-07 20:49:18 -0500

    v2

--- DiscordLinkEditorBot.py
+++ DiscordLinkEditorBot.py
@@ -7,40 +7,49 @@ from dotenv import load_dotenv
 load_dotenv('bot.env')
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+intents.messages = True
+
+client = discord.Client(intents = intents)
 
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-original_author = None
 
 @client.event
 async def on_message(message):
-
-    global original_author
-    original_author = message.author
 
     if message.author == client.user:
         return
 
     if message.content.startswith('https://twitter'):
         output = str(
-            f'sent by {original_author.name}\n' + message.content.replace('https://twitter', 'https://vxtwitter'))
+            f'sent by {message.author.name}\n' + message.content.replace('https://twitter', 'https://vxtwitter'))
         await message.channel.send(output)
         await message.delete()
     elif message.content.startswith('https://x'):
         output = str(
-            f'sent by {original_author.name}\n' + message.content.replace('https://x', 'https://vxtwitter'))
+            f'sent by {message.author.name}\n' + message.content.replace('https://x', 'https://vxtwitter'))
         await message.channel.send(output)
         await message.delete()
 
     # Check if the message is a reply and matches the bot's edited message
     if message.reference:
         referenced_message = await message.channel.fetch_message(message.reference.message_id)
-        if referenced_message.author == client.user and referenced_message.content.startswith(f'sent by {original_author.name}'):
-            await message.channel.send(original_author.mention)
+        if message.reference.resolved.author == client.user:
+            original_author_name = message.reference.resolved.content.split('\n')[0][8:]
+
+            for guild in client.guilds:
+                await guild.fetch_members(limit=None).flatten()
+                original_author = discord.utils.get(guild.members, name = original_author_name)
+                reply_author = str(message.author.mention)
+
+                if original_author:
+                    await message.channel.send(f"{original_author.mention}, {reply_author[1:]} has replied to your link")
+                    break
 
 client.run(TOKEN)
 
@@ -52,4 +61,4 @@ client.run(TOKEN)
 # and add which user sent message in bot's message
 #DELETE PINGED MSG ONCE OP REPLIES TO QUOTED MSG
 #message.author.mention to ping op
-#what happens if someone else uses the bot, will the original_author variable be overwritten? and so it will now reference the latest user of the bot no matter which message you ping
+#use nickname in the original edit instead of username
